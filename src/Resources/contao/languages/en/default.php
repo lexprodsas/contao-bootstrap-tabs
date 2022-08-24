f00fc54da41d83ebf64e9b445a03b4721ce0f1ba
Alexandre Normand <alexandre.n@lexprod.net>
2022-08-24 13:36:17 +0200

    Update default.php

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -6,4 +6,4 @@ $GLOBALS['TL_LANG']['CTE']['lexprodsasBootstrapTabs'] = [
     'Tabs system',
     'Create a content with tabs system',
 ];
-$GLOBALS['TL_LANG']['MSC']['ow_key'] = 'Nav item';
+$GLOBALS['TL_LANG']['tl_content']['data'][0] = 'Config des tabs';
