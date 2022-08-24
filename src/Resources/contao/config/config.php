38cd8bba5c090b3169589844ed44d87769230c5b
Alexandre Normand <alexandre.n@lexprod.net>
2022-08-24 16:55:47 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,7 +5,5 @@ $GLOBALS['TL_CTE']['lexprodsasBootstrap'] = [
 ];
 
 if (TL_MODE === 'FE') {
-    $GLOBALS['TL_JAVASCRIPT'][] = 'assets/contao-component-bootstrap/bootstrap/js/dist/util/config.js';
-    $GLOBALS['TL_JAVASCRIPT'][] = 'assets/contao-component-bootstrap/bootstrap/js/dist/base-component.js';
-    $GLOBALS['TL_JAVASCRIPT'][] = 'assets/contao-component-bootstrap/bootstrap/js/dist/tab.js';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'assets/contao-component-bootstrap/bootstrap/dist/js/bootstrap.js';
 }
