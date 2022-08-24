a2a59de03212d4561aadc5e620f7043830a68cfb
Alexandre Normand <alexandre.n@lexprod.net>
2022-08-24 15:02:18 +0200

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -2,4 +2,4 @@
 
 
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['lexprodsasBootstrapTabs'] = '{type_legend},type;{template_legend},data,customTpl';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['lexprodsasBootstrapTabs'] = '{type_legend},type;{template_legend},data,customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
